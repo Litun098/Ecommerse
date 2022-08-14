8d9662280f2ded236063702000ee217b92beaefc
Litun098 <nayaklitun9@gmail.com>
2022-08-14 10:54:28 +0530

    Roles

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { sequelize, Catagories,Products,User,Role } = require('./models');
+const { sequelize, Catagories, Products, User, Role } = require('./models');
 require('dotenv').config();
 const { serverPort } = require('./config/config.server');
 const routes = require('./routes');
@@ -14,7 +14,7 @@ app.use(routes)
 app.listen(serverPort, async () => {
     console.log('Ecommerse is running at ' + serverPort)
 
-    
+
     // To sync all models
     // await sequelize.sync({force:true});
 
@@ -25,57 +25,66 @@ app.listen(serverPort, async () => {
 
     // await User.sync({force:true});
     // await Role.sync({force:true});
-    
+
     await sequelize.authenticate();
-    // await init()
+    await init()
 })
 
-// async function init(){
-// 	try{
-// 		await sequelize.sync({force:true})
+async function init() {
+    try {
+        await sequelize.sync({ force: true })
 
-// 		const defaultProducts = [
-// 		{
-// 		    "description":"Nyka best products",
-// 		    "name" :"MakeUP Kit",
-// 		    "cost": 870,
-// 		    "quantity": 20,
-// 			"CatagoryId": 1
-// 		},
-// 		{
-//     		"description":"Best fragnance",
-// 		    "name" :"Fogg",
-// 		    "cost": 280,
-// 		    "quantity": 20,
-// 			"CatagoryId": 2
-// 		},
-// 		{
-//     		"description":"Best for summer holidays",
-// 		    "name" :"Summer Clothes",
-// 		    "cost": 1200,
-// 		    "quantity": 20,
-// 			"CatagoryId": 3
-// 		}
-// ]
+        const defaultProducts = [
+            {
+                "description": "Nyka best products",
+                "name": "MakeUP Kit",
+                "cost": 870,
+                "quantity": 20,
+                "CatagoryId": 1
+            },
+            {
+                "description": "Best fragnance",
+                "name": "Fogg",
+                "cost": 280,
+                "quantity": 20,
+                "CatagoryId": 2
+            },
+            {
+                "description": "Best for summer holidays",
+                "name": "Summer Clothes",
+                "cost": 1200,
+                "quantity": 20,
+                "CatagoryId": 3
+            }
+        ]
 
-// 		const defaultCategories = [
-// 		{
-// 			name : 'Beauty',
-// 			description: 'All beauty Products'
-// 		},
-// 		{
-// 			name: 'Fragnance',
-// 			description: 'All Fragnance Products'
-// 		},
-// 		{
-// 			name: 'Clothes',
-// 			description: 'All types of Clothes'
-// 		}
-// 		]
-// 		await Catagories.bulkCreate(defaultCategories)
-// 		await Products.bulkCreate(defaultProducts)
-// 	}
-// 	catch(err){
-// 		console.log(err)
-// 	}
-// }
+        const defaultCategories = [
+            {
+                name: 'Beauty',
+                description: 'All beauty Products'
+            },
+            {
+                name: 'Fragnance',
+                description: 'All Fragnance Products'
+            },
+            {
+                name: 'Clothes',
+                description: 'All types of Clothes'
+            }
+        ]
+        const defaultRoles = [
+            {
+                name: 'User'
+            },
+            {
+                name: 'Admin'
+            }
+        ]
+        await Catagories.bulkCreate(defaultCategories)
+        await Products.bulkCreate(defaultProducts)
+        await Role.bulkCreate(defaultRoles)
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
