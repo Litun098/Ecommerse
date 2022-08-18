174c3ab9190cac19355e8120d549caf3e9effefa
Litun098 <nayaklitun9@gmail.com>
2022-08-18 16:46:22 +0530

    Added authentication to cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const {updateCart,getCart} = require('../controller/cart')
+const {verifyToken} = require('../middleware')
 const routes = express.Router()
 
 
 
-routes.get('/ecomm/api/v1/cart/:id',getCart)
-routes.put('/ecomm/api/v1/cart/:id',updateCart)
+routes.get('/ecomm/api/v1/cart/:id',verifyToken,getCart)
+routes.put('/ecomm/api/v1/cart/:id',verifyToken,updateCart)
 
 module.exports ={ 
     cartRoutes:routes
